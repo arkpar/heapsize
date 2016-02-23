ac6f55a885c63296c37c5345a1b22daef69862d1
Simon Sapin <simon.sapin@exyr.org>
2016-02-24 00:01:50 +0100

    Fix build script when the rustc commit date is unknown.

--- build.rs
+++ build.rs
@@ -10,7 +10,7 @@ fn main() {
         .output()
         .unwrap()
         .stdout;
-    let captures = regex::Regex::new(r"rustc (\d+)\.(\d+)\.(\d+).+(\d{4}-\d{2}-\d{2})\)")
+    let captures = regex::Regex::new(r"rustc (\d+)\.(\d+)\.(\d+).*(\d{4}-\d{2}-\d{2})?")
         .unwrap()
         .captures(str::from_utf8(&version_line).unwrap())
         .unwrap();
@@ -18,7 +18,9 @@ fn main() {
         captures[1].parse::<u32>().unwrap(),
         captures[2].parse::<u32>().unwrap(),
         captures[3].parse::<u32>().unwrap(),
-        &captures[4],
+        // "unknown" sorts after "2016-02-14", defaut to unprefixed
+        // https://github.com/servo/heapsize/pull/44#issuecomment-187935883
+        captures.at(4).unwrap_or("unknown"),
     );
     if version < (1, 8, 0, "2016-02-14") {
         println!("cargo:rustc-cfg=prefixed_jemalloc");
